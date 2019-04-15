7c24735dd847ba52fd17a57904ae8cdcd0784b29
cpj509 <abzi110@naver.com>
2019-04-15 16:33:23 +0900

    Update scraping.py
    keyword를 프로그램 실행시 입력 기능, 해당 keyword 이름의 문서로 저장 기능 추가.

--- src/webcrawling/scraping/scraping.py
+++ src/webcrawling/scraping/scraping.py
@@ -1,33 +1,38 @@
 import time
 import re
+import csv
 from selenium import webdriver
-from datetime import datetime
-now = datetime.now()
+
+tag = input("input keyword :: ")
+f = open(tag + '.txt', 'w', encoding='UTF-8', newline='')
+wr = csv.writer(f)
 #invisible chromedriver
 options = webdriver.ChromeOptions()	
 options.add_argument('headless')	
-options.add_argument('disable-gpu')	
-url = "https://www.instagram.com/explore/tags/%EA%B5%AD%EB%AF%BC%EB%8C%80/"
+options.add_argument('disable-gpu')
+url = "https://www.instagram.com/explore/tags/" + tag
 driver = webdriver.Chrome(options=options)
 driver.implicitly_wait(5) 
 driver.get(url)
 driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[1]/div/div/div[1]/div[1]/a/div[1]/div[2]').click()
-hashtag_regex = "(#[0-9a-zA-Z가-힣]*)"
+hashtag_regex = "#([0-9a-zA-Z가-힣]*)"
 p = re.compile(hashtag_regex)
 Scrapcount = 1
-while Scrapcount <= 20:
+while Scrapcount <= 3:
     try:
         hashtagtext = driver.find_element_by_class_name('k59kT').text
-        #find hashtag in text
         r = p.findall(hashtagtext)
-        print(r)
+        print(".")
+        wr.writerow(r)
     except:
+        print("pass")
         pass
     if Scrapcount == 1:
         driver.find_element_by_xpath('/html/body/div[4]/div[1]/div/div/a').click()
     else:
         driver.find_element_by_xpath('/html/body/div[4]/div[1]/div/div/a[2]').click()
-    time.sleep(2)
+    time.sleep(1)
     Scrapcount += 1
-print ( '%s-%s-%s' % ( now.year, now.month, now.day ) )
+print("complete")
+f.close()
 driver.close()
