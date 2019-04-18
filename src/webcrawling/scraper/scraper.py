bbfa38473bf201c4bbac7b7a9e175f28b072872f
cpj509 <abzi110@naver.com>
2019-04-18 23:05:54 +0900

    코드 수정 및 테스트 문서 업로드
    -실행 지연 코드 수정
    -저장 문서 형식 txt->csv 변경
    -웹 스크래핑 100회, 200회 테스트 및 결과 문서 업로드

--- src/webcrawling/scraper/scraper.py
+++ src/webcrawling/scraper/scraper.py
@@ -4,25 +4,28 @@ import csv
 from selenium import webdriver
 
 tag = input("input keyword :: ")
-f = open(tag + '.txt', 'w', encoding='UTF-8', newline='')
+#f = open(tag + '.csv', 'w', encoding='UTF-8', newline='')
+f = open(tag + '.csv', 'w', newline='')
 wr = csv.writer(f)
+
 #invisible chromedriver
 options = webdriver.ChromeOptions()	
 options.add_argument('headless')	
 options.add_argument('disable-gpu')
 url = "https://www.instagram.com/explore/tags/" + tag
 driver = webdriver.Chrome(options=options)
+#driver = webdriver.Chrome()
 driver.implicitly_wait(5) 
 driver.get(url)
 driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[1]/div/div/div[1]/div[1]/a/div[1]/div[2]').click()
-hashtag_regex = "#([0-9a-zA-Z가-힣]*)"
+hashtag_regex = "(#[0-9a-zA-Z가-힣]*)"
 p = re.compile(hashtag_regex)
 Scrapcount = 1
-while Scrapcount <= 3:
+while Scrapcount <= 200:
+    print(Scrapcount)
     try:
         hashtagtext = driver.find_element_by_class_name('k59kT').text
         r = p.findall(hashtagtext)
-        print(".")
         wr.writerow(r)
     except:
         print("pass")
@@ -31,7 +34,7 @@ while Scrapcount <= 3:
         driver.find_element_by_xpath('/html/body/div[4]/div[1]/div/div/a').click()
     else:
         driver.find_element_by_xpath('/html/body/div[4]/div[1]/div/div/a[2]').click()
-    time.sleep(1)
+    time.sleep(1.5)
     Scrapcount += 1
 print("complete")
 f.close()
