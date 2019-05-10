a2e240adb23f2b7f9a0845cb6b92647c308b546e
zjaaod <tjdwn2032@naver.com>
2019-05-10 17:58:12 +0900

    회원가입 cognito 연결

---
+++ cognito/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        region: 'ap-northeast-2:300331274004:userpool/ap-northeast-2_5hR4x3EXk',
+        userPoolId: 'ap-northeast-2_5hR4x3EXk',
+        appClientId: '6306k6j0k54blgo22chk35o0sl'
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
